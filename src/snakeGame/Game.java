c3a230e0bec23e4e8f9dafc5e4461fe822737871
EfthymiaKostaki <43841219+EfthymiaKostaki@users.noreply.github.com>
2019-01-06 16:24:52 +0200

    Update Game.java

--- src/snakeGame/Game.java
+++ src/snakeGame/Game.java
@@ -1,19 +1,7 @@
-package snakeGame;
+package XmasJavaProject;
 
-import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Random;
-
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class Game extends JFrame {
 
@@ -22,20 +10,7 @@ Game() {
     setResizable(false);
     pack();
     setTitle("Catch the Reindeer!");
-    setLocationRelativeTo(null);
+    setLocation(600,100);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
-
-public static void main(String[] args) {
-
-    // Creates a new thread so our GUI can process itself
-    EventQueue.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-            JFrame frame = new Game();
-            frame.pack();
-            frame.setVisible(true);
-        }
-    });
 }
 }
