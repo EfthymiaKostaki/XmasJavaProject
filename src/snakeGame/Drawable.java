0fe3735aa2c819f2d049c8c0b3930ee40f8bfde3
EfthymiaKostaki <43841219+EfthymiaKostaki@users.noreply.github.com>
2019-01-07 17:17:49 +0200

    Create Drawable.java

---
+++ src/snakeGame/Drawable.java
@@ -0,0 +1,40 @@
+package XmasProject
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+/**
+ * An abstract representation of a self-drawable object.
+ *
+ * @author Giorgos Gousios, Diomidis Spinellis
+ */
+public abstract class Drawable {
+
+    /**
+     * The canvas to draw the object onto
+     */
+    protected Graphics2D canvas;
+
+    /**
+     * The canvas's bounds
+     */
+    protected Rectangle bounds;
+
+    /**
+     * Create drawable item
+     *
+     * @param panel The panel to draw the object onto
+     */
+    public Drawable(JPanel panel) {
+        bounds = panel.getBounds();
+        canvas = (Graphics2D)panel.getGraphics();
+    }
+
+    /**
+     * Draws the object onto the canvas
+     *
+     */
+    public abstract void draw(Graphics g);
+}
