4335abaa1afdb9c03dc6e0aba36d08d529d9cc59
EfthymiaKostaki <43841219+EfthymiaKostaki@users.noreply.github.com>
2019-01-07 17:27:55 +0200

    Create SlashSnowFlake.java

---
+++ src/snakeGame/SlashSnowFlake.java
@@ -0,0 +1,42 @@
+package XmasProject;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * A class that animates a slash on a canvas.
+ *
+ * @author Giorgos Gousios, Diomidis Spinellis
+ * @opt nodefillcolor white
+ */
+public class SlashSnowFlake extends SnowFlake {
+
+    /**
+     * Create a snowflake represented by a slash.
+     *
+     * @param panel The panel to draw the object onto
+     */
+    public SlashSnowFlake(JPanel panel) {
+        super(panel);
+        displayChar = '/';
+    }
+
+    /**
+     * Display the slash on the drawing canvas. The slash alternates between
+     * forward slash and backslash depending on the current 'y' coordinate.
+     *
+     * @param g The Graphics object on which we will paint
+     */
+    @Override
+    public void draw(Graphics g) {
+
+        /* / on even lines, \ on odd lines */
+        displayChar = ((coordY % 2) == 0) ? '/' : '\\';
+
+        /* Move by 0 to 10 pixels down*/
+        coordY += (int) (Math.random() * 10);
+
+        // Draw it through the superclass
+        super.draw(g);
+    }
+}
